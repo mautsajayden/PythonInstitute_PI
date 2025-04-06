9d11682eb3eba75326abd18479129f65ec5a2c11
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-04-06 15:18:14 -0400

    Add files via upload

---
+++ 2.5/LAB Comments.py
@@ -0,0 +1,12 @@
+﻿#this program computes the number of seconds in a given number of hours
+
+a = 3 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", a) #printing the number of hours
+# print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+#here we should also 
+print ("Goodbye")
+#this is the end of the program that computes the number of seconds in 3 hour
+print("the number = ", (a * seconds))
