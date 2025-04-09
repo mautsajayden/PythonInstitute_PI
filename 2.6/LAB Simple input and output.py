5fc5fb57831fb3cbd74c189a76a4070ab39bf958
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-04-08 23:01:59 -0400

    Add files via upload

---
+++ 2.6/LAB Simple input and output.py
@@ -0,0 +1,14 @@
+﻿# input a float value for variable a here
+a = float(input("Enter a variable"))
+# input a float value for variable b here
+b = float(input("Enter a variable"))
+
+# output the result of addition 
+print(a + b)
+# output the result of subtraction here
+print (a-b)
+# output the result of multiplication here
+print(a * b)
+# output the result of division here
+print(a / b)
+print("\nThat's all, folks!")
