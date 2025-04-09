855ad0924edc4be11bf088300d9c5851785d6461
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-04-08 23:01:50 -0400

    Add files via upload

---
+++ 2.6/2.6.1.11 LAB Operators and expressions.py
@@ -0,0 +1,12 @@
+﻿hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+# Write your code here.
+n = mins + dura
+l = n
+n //= 60
+min = int(l % 60)
+hour += n
+
+print(hour, min)
